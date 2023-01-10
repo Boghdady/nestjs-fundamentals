4444320ea79ab2dfb5859d4f062184c0ac456968
boghdady <boghdady107@gmail.com>
2023-01-10 22:08:32 +0200

    Validate input data in DTO

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,8 +1,13 @@
+import { IsEmail, IsString, Length } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @Length(3, 20)
   readonly username: string;
+
+  @IsEmail()
   readonly email: string;
+
+  @IsString()
   readonly country: string;
-
-  // no business logic here
 }
-
--- src/users/usersController.ts
+++ src/users/usersController.ts
@@ -2,53 +2,67 @@ import {
   Body,
   Controller,
   Delete,
-  Get, HttpCode, HttpStatus,
+  Get,
+  HttpCode,
+  HttpStatus,
   Param,
+  ParseUUIDPipe,
   Patch,
-  Post, Res
-} from "@nestjs/common";
-import { CreateUserDto } from "./dtos/create-user.dto";
-import { UpdateUserDto } from "./dtos/update-user.dto";
-import { UserEntity } from "./user.entity";
-import {v4 as uuid} from "uuid"
-
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
+import { UserEntity } from './user.entity';
+import { v4 as uuid } from 'uuid';
 
 @Controller('users')
 export class UsersController {
-  private  users: UserEntity[] = [];
+  private users: UserEntity[] = [];
+
   @Get()
   find(): UserEntity[] {
     return this.users;
   }
 
-  @Get(":id")
-  findOne(@Param("id") id: string) : UserEntity {
-  return  this.users.find((user) => user.id === id);
+  @Get(':id')
+  findOne(
+    @Param('id', ParseUUIDPipe)
+    id: string,
+  ): UserEntity {
+    console.log(typeof id);
+
+    return this.users.find((user) => user.id === id);
   }
+
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
-    const newUser : UserEntity = {
+    const newUser: UserEntity = {
       ...createUserDto,
       id: uuid(),
-    }
+    };
     this.users.push(newUser);
 
     return newUser;
   }
 
- @Patch(":id")
-  update(@Param("id") id: string, @Body() updateUserDto: UpdateUserDto) {
+  @Patch(':id')
+  update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
     // 1) find the element index that we want to update
     const index = this.users.findIndex((user) => user.id === id);
     // 2) update the element
-    this.users[index] = {...this.users[index], ...updateUserDto};
+    this.users[index] = { ...this.users[index], ...updateUserDto };
 
     return this.users[index];
   }
 
-  @Delete(":id")
+  @Delete(':id')
   @HttpCode(HttpStatus.NO_CONTENT)
-  remove(@Param("id", ) id: string) {
-  this.users =  this.users.filter((user) => user.id !== id);
+  remove(@Param('id', ParseUUIDPipe) id: string) {
+    this.users = this.users.filter((user) => user.id !== id);
   }
 }
